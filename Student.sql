cd4003013e7c7484479146cfbddde12416106a29
sohamdeshpande404-netizen <sohamdeshpande404@gmail.com>
2026-08-12 12:08:31 +0530

    Create Communique table and insert student records

---
+++ Student.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Communique(
+    Student_ID int,
+    name varchar(20),
+    Designation varchar(40)
+);
+INSERT INTO Student (Roll_Number, name, branch)
+
+VALUES(1, 'Surmayee Deshpande', 'Executive Member'),
+(2, 'Shivtej Ambilduke', 'Executive Member'),
+(3, 'Athrva Jawale' , 'Executive Member');
